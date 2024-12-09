1a6653eb7d4f1f375788b5312ff444cb61c5f848
xayyomovich <jamolovjamshid131@gmail.com>
2024-12-09 23:43:39 +0500

    code preparation to seperate crud

--- backend/crudapp/views.py
+++ backend/crudapp/views.py
@@ -20,25 +20,32 @@ class CartView(APIView):
         cart = request.session.get('cart', [])
         return JsonResponse({'cart': cart}, safe=False)
 
-    def post(self, request):
-        product_id = request.data.get('product_id')
-        product = Products.objects.filter(id=product_id).first()
 
-        if product:
-            cart = request.session.get('cart', [])
-            cart.append({
-                'id': product.id,
-                'name': product.name,
-                'price': str(product.price),
-                'image': product.image.url if product.image else None
-            })
-            request.session['cart'] = cart
-            return JsonResponse({'message': 'Product added to cart', 'cart': cart})
-        return JsonResponse({'error': 'Product not found'}, status=404)
-
-    def delete(self, request):
-        product_id = request.data.get('product_id')
-        cart = request.session.get('cart', [])
-        cart = [item for item in cart if item['id'] != product_id]
-        request.session['cart'] = cart
-        return JsonResponse({'message': 'Product removed from cart', 'cart': cart})
+# class PostCart(APIView):
+#
+#     def post(self, request):
+#
+#
+#
+#     def post(self, request):
+#         product_id = request.data.get('product_id')
+#         product = Products.objects.filter(id=product_id).first()
+#
+#         if product:
+#             cart = request.session.get('cart', [])
+#             cart.append({
+#                 'id': product.id,
+#                 'name': product.name,
+#                 'price': str(product.price),
+#                 'image': product.image.url if product.image else None
+#             })
+#             request.session['cart'] = cart
+#             return JsonResponse({'message': 'Product added to cart', 'cart': cart})
+#         return JsonResponse({'error': 'Product not found'}, status=404)
+#
+#     def delete(self, request):
+#         product_id = request.data.get('product_id')
+#         cart = request.session.get('cart', [])
+#         cart = [item for item in cart if item['id'] != product_id]
+#         request.session['cart'] = cart
+#         return JsonResponse({'message': 'Product removed from cart', 'cart': cart})
